e38e5036ac6759fde38f726ca09c98d1a6bdf39f
Benya Rungsrisutthiwong <st118856@ait.ac.th>
2016-10-13 04:42:46 +0700

    #8 complete cucumber

--- test/controllers/administration_controller_test.rb
+++ test/controllers/administration_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class AdministrationControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   test "should get userlist" do
     get administration_userlist_url
     assert_response :success
